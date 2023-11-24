a4e52d6f5c4e9f10d3c7f6d04dade3dc198c4604
Bruno Henrique <brunodorea@outlook.com.br>
2023-11-24 13:19:53 -0300

    :sparkles: feat: criação do filtro

--- src/pages/compontes/filter/filter.tsx
+++ src/pages/compontes/filter/filter.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { CardBox, FilterContainer, IconContainer, SearchInput } from './filter.styled'
+import { BsSearch } from "react-icons/bs";
+import { useHomeContext } from 'pages/home';
 
 const Filter: React.FC = () => {
+    const { search, handleSearch } = useHomeContext();
   return (
-    <div>filter</div>
+    <FilterContainer>
+        <CardBox>
+            <IconContainer><BsSearch/></IconContainer>
+            <SearchInput placeholder='Pesquise por usuário do github' value={search} onChange={(e) => handleSearch(e.target.value)} />
+        </CardBox>
+    </FilterContainer>
   )
 }
 
